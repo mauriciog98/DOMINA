5caf26f0a50ab6289392d9729dae6322b08dda48
Andres Mauricio <programador@lcnidiomas.com>
2022-07-21 17:00:31 -0500

    solution of 4 and 5

--- five.php
+++ five.php
@@ -1,13 +1,37 @@
 <?php
 
+const MAX_PERIMETER = 1/9;
+const LIMIT_PERIMETER = 4/9;
+
 /**
  * @throws Exception Position not exists
  */
-function getNumberByPosition(int $row_size, int $column_size, int $row_position, int $column_position){
-    if($row_position > $row_size || $column_position > $column_size){
-        throw new Exception('Position greater than size');
+function getNumberByPosition(string $case){
+    if (strpos($case, ' ') === false) {
+        throw new Exception('Case must have blank space');
     }
 
+    $data = explode(' ', $case);
+    $l = intval($data[0]);
 
+    $n = intval($data[1]);
+
+    $area = $l * $l;
+
+    if ($n > $area) {
+        throw new Exception("Can't take $n blocks, area has only $area blocks");
+    }
+
+    $relation = $area/$n;
+
+    if($relation <= MAX_PERIMETER){
+        $perimeter = ($l * 4) + ($n * 4);
+    }elseif($relation <= LIMIT_PERIMETER){
+        $perimeter = ($l * 4) + ($n * 2);
+    }else{
+        $perimeter = $l*4;
+    }
+
+    return $perimeter;
     
 }
--- four.php
+++ four.php
@@ -8,5 +8,22 @@ function getNumberByPosition(int $row_size, int $column_size, int $row_position,
         throw new Exception('Position greater than size');
     }
 
+    $max_number = $row_position + $column_position;
+    $amount = 0;
+    for($i = 0; $i <= $max_number; $i++){
+        $amount += $i;
+    }
+
+    return $amount + $column_position;
 
 }
+
+echo getNumberByPosition(3,2,3,2).PHP_EOL;
+try {
+    echo getNumberByPosition(3, 2, 3, 9) . PHP_EOL;
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+echo getNumberByPosition(3,4,3,4).PHP_EOL;
+echo getNumberByPosition(5,2,5,2).PHP_EOL;
+echo getNumberByPosition(1,6,1,6).PHP_EOL;
